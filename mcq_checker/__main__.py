cc389154dc21d25284234628d4ceab9fc6c0a6bd
Amr Alaa <n1amr1@gmail.com>
2017-05-04 20:10:28 +0200

    move try, except

--- mcq_checker/__main__.py
+++ mcq_checker/__main__.py
@@ -3,12 +3,9 @@ import sys
 import pandas as pd
 
 from mcq_checker import constants
+from mcq_checker.constants import get_train_image_path, get_test_image_path
 from mcq_checker.grader import Grader
 from mcq_checker.utils.csv import load_csv, save_csv
-from mcq_checker.constants import get_train_image_path, get_test_image_path
-
-img_model = None
-img_model_threshed = None
 
 
 def print_errors(errors):
@@ -24,36 +21,34 @@ def train(grader, samples=None):
     errors = []
     i = -1
     total_abs_error = 0
-    try:
-        for t in train_set[['FileName', 'Mark']].itertuples():
-            i += 1
-            if samples and (i not in samples):
-                continue
 
-            sample_file_path = get_train_image_path(t.FileName)
-            expected_mark = t.Mark
-            print(f'[{t.Index + 1:03}/{len(train_set):03}] '
-                  f'{f"{t.Index / len(train_set) * 100:0.1f}":>5}%: '
-                  f'{t.FileName:30}', end='', flush=True)
+    for t in train_set[['FileName', 'Mark']].itertuples():
+        i += 1
+        if samples and (i not in samples):
+            continue
 
-            output_mark = grader.grade(sample_file_path,
-                                       expected=expected_mark)
+        sample_file_path = get_train_image_path(t.FileName)
+        expected_mark = t.Mark
+        print(f'[{t.Index + 1:03}/{len(train_set):03}] '
+              f'{f"{t.Index / len(train_set) * 100:0.1f}":>5}%: '
+              f'{t.FileName:30}', end='', flush=True)
 
-            output_dataframe.loc[t.Index] = [t.FileName, output_mark]
-            save_csv(output_dataframe, constants.TRAIN_OUTPUT_CSV_FILE_PATH)
+        output_mark = grader.grade(sample_file_path,
+                                   expected=expected_mark)
 
-            error = output_mark - expected_mark
-            total_abs_error += abs(error)
-            print(f'Output = {output_mark:02}, Expected = {expected_mark:02}:'
-                  f' {f"Error = {error}" if error != 0 else "OK":14}'
-                  f'Total absolute error = {total_abs_error:4}')
+        output_dataframe.loc[t.Index] = [t.FileName, output_mark]
+        save_csv(output_dataframe, constants.TRAIN_OUTPUT_CSV_FILE_PATH)
 
-            if error != 0:
-                errors.append({'id': i, 'filename': sample_file_path,
-                               'expected': expected_mark, 'mark': output_mark})
-                print_errors(errors)
-    except KeyboardInterrupt:
-        pass
+        error = output_mark - expected_mark
+        total_abs_error += abs(error)
+        print(f'Output = {output_mark:02}, Expected = {expected_mark:02}:'
+              f' {f"Error = {error}" if error != 0 else "OK":14}'
+              f'Total absolute error = {total_abs_error:4}')
+
+        if error != 0:
+            errors.append({'id': i, 'filename': sample_file_path,
+                           'expected': expected_mark, 'mark': output_mark})
+            print_errors(errors)
 
     print_errors(errors)
 
@@ -64,22 +59,18 @@ def test(grader):
     output_dataframe = load_csv(constants.TEST_OUTPUT_CSV_FILE_PATH,
                                 continue_=False)
 
-    try:
-        for t in test_set[['FileName']].itertuples():
-            sample_file_path = get_test_image_path(t.FileName)
-            print(f'[{t.Index + 1:03}/{len(test_set):03}] '
-                  f'{f"{t.Index / len(test_set) * 100:0.1f}":>5}%: '
-                  f'{t.FileName:30}', end='', flush=True)
+    for t in test_set[['FileName']].itertuples():
+        sample_file_path = get_test_image_path(t.FileName)
+        print(f'[{t.Index + 1:03}/{len(test_set):03}] '
+              f'{f"{t.Index / len(test_set) * 100:0.1f}":>5}%: '
+              f'{t.FileName:30}', end='', flush=True)
 
-            output_mark = grader.grade(sample_file_path)
+        output_mark = grader.grade(sample_file_path)
 
-            output_dataframe.loc[t.Index] = [t.FileName, output_mark]
-            save_csv(output_dataframe, constants.TEST_OUTPUT_CSV_FILE_PATH)
+        output_dataframe.loc[t.Index] = [t.FileName, output_mark]
+        save_csv(output_dataframe, constants.TEST_OUTPUT_CSV_FILE_PATH)
 
-            print(f'Output = {output_mark:02}:')
-
-    except KeyboardInterrupt:
-        pass
+        print(f'Output = {output_mark:02}:')
 
 
 def print_usage():
@@ -107,4 +98,7 @@ def main(*args):
 
 
 if __name__ == '__main__':
-    sys.exit(main(*sys.argv))
+    try:
+        sys.exit(main(*sys.argv))
+    except KeyboardInterrupt:
+        pass
